d71ed16a611e75b91e1a4b8012a0882c5312770f
Messi Lee <hojunlee@wustl.edu>
2023-11-20 12:33:41 -0600

    Data Collection Files
    Upload files relevant to data collection

---
+++ Names/select_names.R
@@ -0,0 +1,24 @@
+
+## Messi Lee, Patrick Farley, Sangwook Suh
+# Project Milestone
+
+## Script date: 18 Nov 2023
+
+# Install and/or load packages -------------------------------------------------
+
+if(!require("tidyverse")){install.packages("tidyverse", dependencies = TRUE); require("tidyverse")}
+
+# Import text ------------------------------------------------------------------
+
+names = read.csv('nameratings.csv')
+
+# Select names by race ---------------------------------------------------------
+
+set.seed(1048596)
+
+sampled_names = names %>% group_by(race, gender) %>% 
+  slice_sample(n = 15) %>% select(c("name", "race", "gender")) %>%
+  ungroup()
+
+# Write the sampled names as a .csv file
+write.csv(sampled_names, "sampled_names.csv", row.names = FALSE)
